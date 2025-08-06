b4e86a409ef2462ced0a10a7e33916681782ca69
RiceCakes27 <furryfoxy27@gmail.com>
2025-08-05 21:58:48 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function updateClock() {
+    let now = new Date();
+
+    document.getElementById('time').textContent = now.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+    document.getElementById('date').textContent = `${now.getMonth()+1}/${now.getDate()}/${now.getFullYear()}`;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
+
+document.querySelectorAll('#start-button, #search-bar, #taskbar-apps div').forEach(el => {
+    el.addEventListener('mousedown', () => {
+        el.classList.add('pressed');
+    });
+    el.addEventListener('mouseup',  () => {
+        el.classList.remove('pressed');
+        el.classList.add('released');
+        setTimeout(function() {
+            el.classList.remove('released');
+        }, 100);
+    });
+});
+
+let clicks = 0;
+document.getElementById('garry').addEventListener('click', () => {
+    clicks++;
+    let oldclicks = clicks;
+    setTimeout(function() {
+        if (clicks === oldclicks) {
+            clicks = 0;
+        }
+    }, 500);
+    if (clicks >= 2) {
+        document.location.href = '/garry/loading.html'
+    }
+});
